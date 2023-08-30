d688e2d6dedaa0f2991a48b2d2e88af63af61983
Yeinier <yeinierv@gmail.com>
2023-08-30 15:01:51 -0400

    Add Nav component

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Menu">Menu</a></li>
+                <li><a href="#Reservations">Reservations</a></li>
+                <li><a href="#Order online">Order online</a></li>
+                <li><a href="#Login">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
