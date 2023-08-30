252c1507894576c908d1a42ef753c082ff5ab54f
Yeinier <yeinierv@gmail.com>
2023-08-30 13:48:44 -0400

    Add Header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header=()=>{
+    return (
+        <header>
+             <img src="logo.png" alt="Logo" height="50px" />
+        </header>
+    )
+}
+
+export default Header;
